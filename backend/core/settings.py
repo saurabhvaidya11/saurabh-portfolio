ff6432da4f8cd0c282a04f9525e28ca0675ed47a
Saurabh Vaidya <saurabhvaidya2003@gmail.com>
2026-03-16 14:45:39 +0530

    Add PostgreSQL database support

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -77,11 +77,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
 
